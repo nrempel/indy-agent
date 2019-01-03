936c3a394a166ac6d07c1e6566dcebbbd4e7f790
Nicholas Rempel <nbrempel@gmail.com>
2019-01-03 15:31:01 -0800

    Add forward msg models
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- indyagent/messages/routing/forward.py
+++ indyagent/messages/routing/forward.py
@@ -1,7 +1,24 @@
+"""
+Represents a forward message.
+"""
+
+from marshmallow import Schema, fields
+
 from ..agent_message import AgentMessage
 from ..message_types import MessageType
 
 
 class Forward(AgentMessage):
+    def __init__(self, to: str, msg: str):
+        self.to = to
+        self.msg = msg
+
     def type(self):
         return MessageType.FORWARD
+
+
+class ForwardSchema(Schema):
+    # Avoid clobbering builtin property
+    _type = fields.Str(data_key="@type")
+    to = fields.Str()
+    msg = fields.Str()

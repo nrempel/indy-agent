b12e64b5527371f1c3c884f564d69b5fc7cf5820
Nicholas Rempel <nbrempel@gmail.com>
2019-01-03 15:29:25 -0800

    Add proof message models
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- indyagent/messages/proofs/proof.py
+++ indyagent/messages/proofs/proof.py
@@ -1,7 +1,24 @@
+"""
+A proof content message.
+"""
+
+from marshmallow import Schema, fields
+
 from ..agent_message import AgentMessage
 from ..message_types import MessageType
 
 
 class Proof(AgentMessage):
+    def __init__(self, proof_json: str, request_nonce: str):
+        self.proof_json = proof_json
+        self.request_nonce = request_nonce
+
     def type(self):
         return MessageType.PROOF
+
+
+class ProofSchema(Schema):
+    # Avoid clobbering builtin property
+    _type = fields.Str(data_key="@type")
+    proof_json = fields.Str()
+    request_nonce = fields.Str()
--- indyagent/messages/proofs/proof_request.py
+++ indyagent/messages/proofs/proof_request.py
@@ -1,7 +1,22 @@
+"""
+A proof request content message.
+"""
+
+from marshmallow import Schema, fields
+
 from ..agent_message import AgentMessage
 from ..message_types import MessageType
 
 
 class ProofRequest(AgentMessage):
+    def __init__(self, proof_request_json: str):
+        self.proof_request_json = proof_request_json
+
     def type(self):
         return MessageType.PROOF_REQUEST
+
+
+class ProofRequestSchema(Schema):
+    # Avoid clobbering builtin property
+    _type = fields.Str(data_key="@type")
+    proof_request_json = fields.Str()
